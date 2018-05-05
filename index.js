754ee1f888887513f0f39a45e60cd81b59982530
Hanut Singh Gusain <hanutsingh@gmail.com>
2018-05-06 04:22:27 +0530

    Cleaned up the code and refactored the comments for JSDoc

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const TwoFactor = function (APIKEY, SENDERID) {
 
 /**
 * Method to retrieve balance for this client
-* @return {Promise} - A promise that resolved with the balance for this account
+* @return {Promise} A promise object that resolves after the api call has completed
 */
 TwoFactor.prototype.balance = function() {
   return new Promise((resolve, reject) => {
@@ -42,7 +42,7 @@ TwoFactor.prototype.balance = function() {
 * @param  {string} message - The message to be sent
 * @param  {string} [senderId] - A senderId to be used while sending this message otherwise the defaultSID is used
 * @param  {date} [sendAt] - time to schedule this SMS for
-* @return {Promise} - A promise that resolves with the status of the request or rejects with an error
+* @return {Promise} A promise object that resolves after the api call has completed
 */
 TwoFactor.prototype.sendTransactional = function(phoneNumbers, message, senderId, sendAt) {
   return new Promise((resolve, reject) => {
@@ -78,7 +78,15 @@ TwoFactor.prototype.sendTransactional = function(phoneNumbers, message, senderId
   })
 }
 
-TwoFactor.prototype.sendTemplate = function(phoneNumbers, templateName, values, senderId) {
+/**
+ * Send a template based SMS with pre approved senderid
+ * @param  {array} phoneNumbers - An array of phone numbers without country codes
+ * @param  {string} templateName - Name of the template to be used
+ * @param  {string} values - An array of variables to be used in the template
+ * @param  {string} [senderId] - Optional parameter to specify a different senderid for this request
+ * @return {Promise} A promise object that resolves after the api call has completed
+ */
+ TwoFactor.prototype.sendTemplate = function(phoneNumbers, templateName, values, senderId) {
   return new Promise((resolve, reject) => {
     if (typeof phoneNumbers === 'undefined' || typeof templateName === 'undefined') {
       reject(new Error('sendTemplate() expects phoneNumbers and message to be specified'))
