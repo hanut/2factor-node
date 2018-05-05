b4ff27b1aa4e4844fc11fc6091e916504d1d8441
Hanut Singh Gusain <hanutsingh@gmail.com>
2018-05-06 03:51:37 +0530

    Added test cases for sendTemplate and functionality for the same. Also renamed send to sendTransactional. Also added the template for mocha testing to package.json. The APIKEY and TESTPHONE values are now picked up from the environment variables instead of being hardcoded

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 const request = require('request')
 
 /**
- * Constructor function that takes an API key and
- * an optional default senderid as parameters and returns
- *
- * @param {[type]} APIKEY   [description]
- * @param {[type]} SENDERID [description]
- */
+* Constructor function that takes an API key and
+* an optional default senderid as parameters and returns
+* a new TwoFactor api client.
+*
+* @param {string} APIKEY - The apikey to be used while making requests.
+* @param {string} [SENDERID=TFCTOR] - default senderid to be used if no sender id
+* is specified in send().
+*/
 const TwoFactor = function (APIKEY, SENDERID) {
   this.baseURL = "https://2factor.in/API/V1/" + APIKEY + "/ADDON_SERVICES"
   this.defaultSID = SENDERID || "TFCTOR"
 }
 
+/**
+* Method to retrieve balance for this client
+* @return {Promise} - A promise that resolved with the balance for this account
+*/
 TwoFactor.prototype.balance = function() {
   return new Promise((resolve, reject) => {
     var req = request.get({
@@ -30,18 +36,69 @@ TwoFactor.prototype.balance = function() {
   })
 }
 
-TwoFactor.prototype.send = function(phone, message, senderid) {
+/**
+* Send a transactional, open template SMS
+* @param  {array} phoneNumbers - An array of phone numbers without country codes
+* @param  {string} message - The message to be sent
+* @param  {string} [senderId] - A senderId to be used while sending this message otherwise the defaultSID is used
+* @param  {date} [sendAt] - time to schedule this SMS for
+* @return {Promise} - A promise that resolves with the status of the request or rejects with an error
+*/
+TwoFactor.prototype.sendTransactional = function(phoneNumbers, message, senderId, sendAt) {
   return new Promise((resolve, reject) => {
-    if (typeof phone === 'undefined' || typeof message === 'undefined') {
-      reject(new Error('send(phone, message) expects phone and message to be specified'))
+    if (typeof phoneNumbers === 'undefined' || typeof message === 'undefined') {
+      reject(new Error('sendTransactional() expects phoneNumbers and message to be specified'))
     } else {
+      if(!Array.isArray(phoneNumbers)) {
+        phoneNumbers = [phoneNumbers]
+      }
+      let payload = {
+        From: senderId || this.defaultSID,
+        To: phoneNumbers.join(","),
+        Msg: message
+      }
+      if (sendAt) {
+        payload.SendAt = sendAt
+      }
       var req = request.post({
         url: this.baseURL + "/SEND/TSMS",
-        form: {
-          From: senderid || this.defaultSID,
-          To: phone,
-          Msg: message
+        form: payload
+      }, function(err, res, body) {
+        if (err) {
+          reject(err);
         }
+        try {
+          let parsedBody = JSON.parse(body)
+          resolve(parsedBody)
+        } catch(e) {
+          reject(e)
+        }
+      })
+    }
+  })
+}
+
+TwoFactor.prototype.sendTemplate = function(phoneNumbers, templateName, values, senderId) {
+  return new Promise((resolve, reject) => {
+    if (typeof phoneNumbers === 'undefined' || typeof templateName === 'undefined') {
+      reject(new Error('sendTemplate() expects phoneNumbers and message to be specified'))
+    } else {
+      if(!Array.isArray(phoneNumbers)) {
+        phoneNumbers = [phoneNumbers]
+      }
+      let payload = {
+        From: senderId || this.defaultSID,
+        To: phoneNumbers.join(","),
+        TemplateName: templateName
+      }
+      if(Array.isArray(values)) {
+        values.forEach((element, index) => {
+          payload['VAR' + (index + 1)] = element
+        })
+      }
+      var req = request.post({
+        url: this.baseURL + "/SEND/TSMS",
+        form: payload
       }, function(err, res, body) {
         if (err) {
           reject(err);
--- tests/module.spec.js
+++ tests/module.spec.js
@@ -1,6 +1,8 @@
 const APIKEY = process.env.APIKEY || ''
-if (APIKEY === '') {
-  throw new Error()
+const TESTPHONE = process.env.TESTPHONE || ''
+if (APIKEY === '' || TESTPHONE === '') {
+  throw new Error('Invalid or missing APIKEY or TESTPHONE')
+  process.exit(1)
 }
 const expect = require('chai').expect
 const TwoFactor = new (require('../index'))(APIKEY)
@@ -44,17 +46,17 @@ describe('2Factor API module', function() {
     })
   })
 
-  describe('#send()', function() {
+  describe('#sendTransactional()', function() {
     it('should be a function', function() {
-      expect(TwoFactor.send).is.a('function')
+      expect(TwoFactor.sendTransactional).is.a('function')
     })
     it('should return a promise', function() {
-      expect(TwoFactorWrongKey.send(123, 'abc')).is.a('promise')
+      expect(TwoFactorWrongKey.sendTransactional(123, 'abc')).is.a('promise')
     })
 
-    context('If some parameters are missing', function() {
+    context('If phone numbers or message are missing', function() {
       it('should reject with an error message', function(done) {
-        TwoFactorWrongKey.send().then((response) => {
+        TwoFactorWrongKey.sendTransactional().then((response) => {
           done(new Error('did not reject an invalid function call'))
         }, (error) => {
           done()
@@ -64,7 +66,7 @@ describe('2Factor API module', function() {
 
     context('Invalid API Key', function() {
       it('should convert response string to json and handle invalid apikey error', function(done) {
-        TwoFactorWrongKey.send(7016907360, 'testing').then((response) => {
+        TwoFactorWrongKey.sendTransactional(TESTPHONE, 'testing').then((response) => {
           if (response.Status.match(/Success/)) {
             done('2Factor API allowed invalid key')
           } else {
@@ -78,12 +80,54 @@ describe('2Factor API module', function() {
 
     context('Valid API Key', function() {
       it('should return Status as success and status in Details field', function(done) {
-        TwoFactor.send(7016907360, 'test').then((response) => {
+        TwoFactor.sendTransactional(TESTPHONE, 'test').then((response) => {
           expect(response).is.an('object')
           expect(response).has.keys(['Status', 'Details'])
-          console.log(response)
-          // expect(response.Status).is.equal('Success')
-          // expect(+response.Details).is.a('number')
+          done()
+        }, (error) => {
+          done(error)
+        })
+      })
+    })
+  })
+
+  describe('#sendTemplate()', function() {
+    it('should be a function', function() {
+      expect(TwoFactor.sendTemplate).is.a('function')
+    })
+    it('should return a promise', function() {
+      expect(TwoFactorWrongKey.sendTemplate(123, 'abc')).is.a('promise')
+    })
+
+    context('If phone numbers or template name are missing', function() {
+      it('should reject with an error message', function(done) {
+        TwoFactorWrongKey.sendTemplate().then((response) => {
+          done(new Error('did not reject an invalid function call'))
+        }, (error) => {
+          done()
+        })
+      })
+    })
+
+    context('Invalid API Key', function() {
+      it('should convert response string to json and handle invalid apikey error', function(done) {
+        TwoFactorWrongKey.sendTemplate(TESTPHONE, 'testing').then((response) => {
+          if (response.Status.match(/Success/)) {
+            done('2Factor API allowed invalid key')
+          } else {
+            done()
+          }
+        }).catch((error) => {
+          done(error)
+        })
+      })
+    })
+
+    context('Valid API Key', function() {
+      it('should return Status as success and status in Details field', function(done) {
+        TwoFactor.sendTemplate(TESTPHONE, 'test', ['Sweety','https://webclient.patchus.in/regenerate/test']).then((response) => {
+          expect(response).is.an('object')
+          expect(response).has.keys(['Status', 'Details'])
           done()
         }, (error) => {
           done(error)
